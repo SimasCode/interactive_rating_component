16582a4efaf31bf59b0ff10404d6a42a2603e9c9
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-09-03 20:49:31 +0300

    from rate-container to thanks-container switch

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
 console.log('index.js file was loaded');
+
+const form = document.getElementById('form');
+const resultValue = document.getElementById('rated-value');
+const rateContainer = document.getElementById('rate-container');
+const thanksContainer = document.getElementById('thanks-container');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const selectedValue = document.querySelector("input[name='rate']:checked");
+
+  resultValue.textContent = selectedValue.value;
+
+  rateContainer.classList.add('hide');
+  thanksContainer.classList.remove('hide');
+});
